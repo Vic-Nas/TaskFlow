16b740ea3247da1a7827d2e725a57b654efa83e9
Vic-Nas <nasci.victorio@gmail.com>
2025-08-08 12:57:52 -0400

    No update when using python

--- TaskFlow.py
+++ TaskFlow.py
@@ -3,11 +3,20 @@
 from src.main import main
 import platform
 from pymsgbox import alert
+import os, sys
 
-try:
-    name = "update" + platform.system()
-    exec(f"from src.main import {name};{name}()")
-except Exception as e:
-    alert(f"Update unavailabale:\n{e}")
+
+filename = os.path.basename(sys.argv[0])
+
+if not filename.lower().endswith('.py'):
+    try:
+        name = "update" + platform.system()
+        exec(f"from src.main import {name};{name}()")
+    except Exception as e:
+        alert(f"Update unavailabale:\n{e}")
+else:
+    message = "Auto update disabled when using python.\n"
+    message += "You'll have to do redownload the src folder."
+    alert(message)
 
 main()
